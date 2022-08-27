72528cbab35db9543241324e0af8a6fc71ac03b3
Serg <sergeyrs@bk.ru>
2022-08-28 01:35:13 +0700

    done

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,15 @@
 <template>
-  <main v-if="!loading"><DataTitle :text="title" :dataDate="dataDate" />
-  <DataBoxes :stats="stats" />
+  <main v-if="!loading">
+    <DataTitle :text="title" :dataDate="dataDate" />
+    <DataBoxes :stats="stats" />
+    <CountrySelect :countries="countries" @get-country="getCountryData" />
+    <button
+      @click="clearCountryData"
+      v-if="stats.Country"
+      class="bg-green-700 text-white rounded p-3 mt-10 focus:outline-none hover:bg-green-600"
+    >
+      Clear Country
+    </button>
   </main>
   <main class="flex flex-col align-center justify-center text-center" v-else>
     <div class="text-gray-500 text-3xl mt-10 mb-6">Fetching Data</div>
@@ -11,10 +20,11 @@
 <script>
 import DataTitle from "@/components/DataTitle.vue";
 import DataBoxes from "@/components/DataBoxes.vue";
+import CountrySelect from "@/components/CountrySelect.vue";
 
 export default {
   name: "HomeView",
-  components: { DataTitle, DataBoxes, DataBoxes },
+  components: { DataTitle, DataBoxes, DataBoxes, CountrySelect },
   data() {
     return {
       loading: true,
@@ -30,6 +40,17 @@ export default {
       const res = await fetch("https://api.covid19api.com/summary");
       const data = await res.json();
       return data;
+    },
+    getCountryData(country) {
+      this.stats = country;
+      this.title = country.Country;
+    },
+    async clearCountryData() {
+      this.loading = true;
+      const data = await this.fetchCovidData();
+      this.title = "Global";
+      this.stats = data.Global;
+      this.loading = false;
     },
   },
   async created() {
